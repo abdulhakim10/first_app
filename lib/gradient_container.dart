99392bb1468ee84d9b97db76aa2a99f384008be8
syed abdul hakim <s.abdulhakim10@gmail.com>
2023-08-05 03:42:47 +0600

    practice constructor function and reusable widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,28 +1,26 @@
+import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
           gradient: LinearGradient(
-        colors: [
-          Color.fromARGB(255, 26, 5, 120),
-          Color.fromARGB(240, 45, 6, 123)
-        ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
+        colors: [color1, color2],
+        begin: startAlignment,
+        end: endAlignment,
       )),
       child: const Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
+        child: StyleText('Hiee World'),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Scaffold(body: GradientContainer()),
+    home: Scaffold(
+        body: GradientContainer(
+            Color.fromARGB(235, 35, 3, 65), Color.fromARGB(188, 66, 5, 163))),
   ));
 }
